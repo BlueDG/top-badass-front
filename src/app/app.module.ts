3dadd707ea1236226567d38ddd240eaaba73715e
BlueDG <Guillaume.dusseuxgicquel@gmail.com>
2019-08-13 15:17:01 +0200

    character-list-done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { HelloComponent } from './hello/hello.component';
-import { HelloWithNameComponent } from './hello-with-name/hello-with-name.component';
 import { ReviewComponent } from './review/review.component';
 import { CharacterComponent } from './character/character.component';
 import { CharacterListComponent } from './character-list/character-list.component';
-import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 
 
@@ -18,12 +15,9 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
-    HelloWithNameComponent,
     ReviewComponent,
     CharacterComponent,
     CharacterListComponent,
-    NavComponent,
     HomeComponent
   ],
   imports: [
